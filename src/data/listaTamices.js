046355859643a42959f7c32a4ab1136cccc168de
Dario Villar <dario_villar2001@hotmail.com>
2022-04-24 00:44:14 -0300

    Unidad 2 con sistema de suelos SUCS

---
+++ src/data/listaTamices.js
@@ -0,0 +1,127 @@
+export const listaTamices = [
+    {
+        'id': 0,
+        'nombre': '4"',
+        'granulometria': '100',
+    },
+    {
+        'id': 1,
+        'nombre': '3 1/2"',
+        'granulometria': '88.90',
+    },
+    {
+        'id': 2,
+        'nombre': '3"',
+        'granulometria': '76.20',
+    },
+    {
+        'id': 3,
+        'nombre': '2 1/2"',
+        'granulometria': '63.50',
+    },
+    {
+        'id': 4,
+        'nombre': '2"',
+        'granulometria': '50.80',
+    },
+    {
+        'id': 5,
+        'nombre': '1 3/4"',
+        'granulometria': '44.45',
+    },
+    {
+        'id': 6,
+        'nombre': '1 1/2"',
+        'granulometria': '38.10',
+    },
+    {
+        'id': 7,
+        'nombre': '1 1/4"',
+        'granulometria': '31.75',
+    },
+    {
+        'id': 8,
+        'nombre': '1"',
+        'granulometria': '25.40',
+    },
+    {
+        'id': 9,
+        'nombre': '3/4"',
+        'granulometria': '19.05',
+    },
+    {
+        'id': 10,
+        'nombre': '1/2"',
+        'granulometria': '12.70',
+    },
+    {
+        'id': 11,
+        'nombre': '1/4"',
+        'granulometria': '6.35',
+    },
+    {
+        'id': 12,
+        'nombre': 'N°4',
+        'granulometria': '4.75',
+    },
+    {
+        'id': 13,
+        'nombre': '1/8"',
+        'granulometria': '3.17',
+    },
+    {
+        'id': 14,
+        'nombre': 'N°8',
+        'granulometria': '2.36',
+    },
+    {
+        'id': 15,
+        'nombre': 'N°10',
+        'granulometria': '2.00',
+    },
+    {
+        'id': 16,
+        'nombre': 'N°16',
+        'granulometria': '1.18',
+    },
+    {
+        'id': 17,
+        'nombre': 'N°20',
+        'granulometria': '0.850',
+    },
+    {
+        'id': 18,
+        'nombre': 'N°30',
+        'granulometria': '0.600',
+    },
+    {
+        'id': 19,
+        'nombre': 'N°40',
+        'granulometria': '0.425',
+    },
+    {
+        'id': 20,
+        'nombre': 'N°60',
+        'granulometria': '0.250',
+    },
+    {
+        'id': 21,
+        'nombre': 'N°100',
+        'granulometria': '0.150',
+    },
+    {
+        'id': 22,
+        'nombre': 'N°200',
+        'granulometria': '0.075',
+    },
+    {
+        'id': 23,
+        'nombre': 'N°270',
+        'granulometria': '0.053',
+    },
+    {
+        'id': 24,
+        'nombre': 'N°450',
+        'granulometria': '0.032',
+    },
+]
